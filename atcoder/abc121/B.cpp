482085d4ddb1f59ef0b62ca81ac5f4b4faa95944
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2019-03-10 20:02:00 +0100

    Add solution for problem `B - Can you solve this?` Link: https://atcoder.jp/contests/abc121/tasks/abc121_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc121/submissions/4545899

---
+++ atcoder/abc121/B.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+
+
+#define F first
+#define S second
+
+#define ERR 1e-9
+
+#if !ONLINE_JUDGE && !EVAL
+    #define dbg_var(x) cerr << #x << ": " << x << "\n";
+    #define dbg_iter(x, y) cerr  << #x << ": " << print_iterable(x, y) << "\n";
+#else
+    #define dbg_var(x)
+    #define dbg_iter(x, y)
+#endif // ONLINE_JUDGE
+
+template <typename T1, typename T2>
+string print_iterable( T1 begin_iter, T2 end_iter){
+    bool first = true;
+    stringstream res;
+    res << "[ ";
+    for(; begin_iter != end_iter; ++begin_iter){
+        if(!first) res << ", ";
+        first = false;
+        res << *begin_iter;
+    }
+    res << " ]";
+    return res.str();
+}
+
+void aggMax(int & res, int x){ if(x > res)res = x; }
+void aggMin(int & res, int x){ if(x < res)res = x; }
+
+int II(){ int i; cin >> i; return i; }
+void OI(int i){ cout << i; }
+
+// constraints
+#define MAXN 1000000
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N = II(), M = II(), C = II();
+    vi B(M);
+    for(int & x : B) x = II();
+    int res = 0;
+    for(int i=0; i<N; i++){
+        int tmp = C;
+        for(int j=0; j<M; j++){
+            int A = II();
+            tmp += A*B[j];
+        }
+        if(tmp > 0) res++;
+    }
+    OI(res);
+    return 0;
+}
