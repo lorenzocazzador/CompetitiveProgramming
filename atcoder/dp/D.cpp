2eb1222a46785abe7030a133ad8abb362a582654
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2019-03-19 23:20:00 +0100

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/4636604

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int,int> pii;
+typedef long long ll;
+typedef pair<int, int> pii;
 #define F first
 #define S second
 
-int main() {
+const int MAXN = 100, MAXW = 10e5 + 10;
+ll dp[MAXN][MAXW];
+
+int main(){
     int N, W;
     cin >> N >> W;
-    vector<pii> O(N);
-    for(pii & x : O)
-        cin >> x.F >> x.S;
-    vector<long long> dp(W+1, 0);
-    for(pii & x : O) 
-        for(int w=W; w-x.F>=0; w--)
-            dp[w] = max(dp[w], dp[w-x.F]+x.S); 
-    cout << dp[W];
+
+    vector<pii> obj(N);
+
+    for(int i=0; i<N; i++) cin >> obj[i].F >> obj[i].S;
+
+    /// initialize the table
+    for(int j=0; j<=W; j++) dp[0][j] = (j >= obj[0].F) ? obj[0].S : 0;
+
+    for(int i=1; i<N; i++){
+        for(int j=0; j<=W; j++){
+            if(j < obj[i].F) dp[i][j] = dp[i-1][j];
+            else dp[i][j] = max(dp[i-1][j], dp[i-1][j-obj[i].F] + obj[i].S);
+        }
+    }
+
+    cout << dp[N-1][W] << "\n";
 }
