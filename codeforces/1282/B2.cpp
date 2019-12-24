035c21629d2a2e85f83dd8922e9c9fe92e487227
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2019-12-24 19:37:00 +0100

    Add solution for problem `B2 - K for the Price of One (Hard Version)` Link: https://codeforces.com/contest/1282/problem/B2 Tags: dp, greedy, sortings, *1600 Ref: https://codeforces.com/contest/1282/submission/67564278

---
+++ codeforces/1282/B2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T; 
+    cin >> T;
+    
+    while(T--) {
+        int N, P, K;
+        cin >> N >> P >> K;
+
+        vector<int> A(N);
+        for(int i=0; i<N; i++)
+            cin >> A[i];
+        
+        sort(A.begin(), A.end());
+        int res = 0;
+        for(int i=0; i<N; i++) {
+            if(i >= K)
+                A[i] += A[i-K];
+            else if(i && i != K-1)
+                A[i] += A[i-1];
+                
+            if(A[i] <= P)
+                res = i+1;
+        }
+
+        cout << res << "\n";
+    }
+}
