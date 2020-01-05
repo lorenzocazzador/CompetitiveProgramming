e94a5e562f339311f82766f4195e9b108f413a2a
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-01-05 11:30:00 +0100

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/9324272

--- atcoder/dp/A.cpp
+++ atcoder/dp/A.cpp
@@ -1,26 +1,19 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int N ;
-int memo[100010];
-
-int dp(int i, int* h) {
-    if(i == N - 1) return 0;
-    if( memo[i] != -1) return memo[i];
-    int a = INT_MAX;
-    if(i < N - 2) a = abs(h[i] - h[i + 2]) + dp(i + 2, h);
-    int b = abs(h[i] - h[i + 1]) + dp(i + 1, h);
-    return  memo[i] = min(a, b);
-}
-
 int main() {
+    int N;
     cin >> N;
-    memset(memo, -1, sizeof memo);
-    int h[N];
-    for(int i = 0; i < N; i++){
-        cin >> h[i]; 
+    vector<int> H(N);
+    for(int & x : H)
+        cin >> x;
+    vector<long long> dp(N, INT_MAX);
+    dp[0] = 0;
+    for(int i=0; i<N; i++) {
+        if(i+1 < N)
+            dp[i+1] = min(dp[i+1], dp[i] + abs(H[i]-H[i+1]));
+        if(i+2 < N)
+            dp[i+2] = min(dp[i+2], dp[i] + abs(H[i]-H[i+2]));
     }
-    cout << dp(0, h);
-    return 0;
+    cout << dp[N-1] << "\n";
 }
