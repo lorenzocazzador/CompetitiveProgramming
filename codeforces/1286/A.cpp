978cb099412f8efe6f328fe9f7fd40dcaf3e1d44
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-01-06 15:48:00 +0100

    Add solution for problem `A - Garland` Link: https://codeforces.com/contest/1286/problem/A Tags: dp, greedy, sortings, *1800 Ref: https://codeforces.com/contest/1286/submission/68320582

--- codeforces/1286/A.cpp
+++ codeforces/1286/A.cpp
@@ -1,34 +1,87 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 110;
-int N, P[MAXN], dp[MAXN][MAXN][MAXN][2];
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+#define F first
+#define S second
 
-int f(int i, int p, int d, int l) {
-    if(i >= N)
-        return 0;
-    if(dp[i][p][d][l] != -1)
-        return dp[i][p][d][l];
-    if(P[i]) {
-        int ans = f(i+1, p, d, P[i]%2);
-        if(l != P[i]%2) ans++;
-        return dp[i][p][d][l] = ans;
-    } else {
-        int ans = INT_MAX;
-        if(p) ans = min(ans, f(i+1, p-1, d, 0) + (l != 0));
-        if(d) ans = min(ans, f(i+1, p, d-1, 1) + (l != 1));
-        return dp[i][p][d][l] = ans;
-    }
+bool comp(pair<int,bool> a, pair<int,bool> b) {
+    if(a.S == b.S)
+        return a.F < b.F;
+    else if(a.S)
+        return false;
+    else 
+        return true;
 }
 
 int main() {
+    int N; 
     cin >> N;
-    int p = N/2, d = (N+1)/2;
-    for(int i=0; i<N; i++) {
-        cin >> P[i];
-        if(P[i] && P[i]%2) d--;
-        else if(P[i]) p--;
+    vi G(N);
+    vector<bool> flag(N+1, false);
+    for(int & x : G) {
+        cin >> x;
+        flag[x] = true;
     }
-    memset(dp, -1, sizeof dp);
-    cout << min(f(0,p,d,0),f(0,p,d,1)) << "\n";
+    int P = 0, D = 0;
+    for(int i=1; i<=N; i++) 
+        if(!flag[i]) {
+            if(i % 2) D++;
+            else P++;
+        }
+    vector<pair<int,bool>> UP, UD;
+    int Div = 0, last = -1;
+    for(int i=0; i<N; i++) {
+        if(G[i] != 0) {
+            if(i != 0) {
+                if(last == -1) {
+                    if(G[i] % 2)
+                        UD.push_back({i,1});
+                    else 
+                        UP.push_back({i,1});
+                }
+                else{
+                    if(G[i] % 2 == G[last] % 2) {
+                        if(G[last] % 2)
+                            UD.push_back({i-last-1,0});
+                        else 
+                            UP.push_back({i-last-1,0});
+                    } else Div++;
+                }
+            }
+            last = i;
+        }
+    }
+    if(last == -1) {
+        cout << (N == 1 ? 0 : 1);
+        return 0;
+    } else if(last != N-1) {
+        if(G[last] % 2) 
+            UD.push_back({N-1-last,1});
+        else
+            UP.push_back({N-1-last,1});
+    } 
+    sort(UP.begin(), UP.end(),comp);
+    sort(UD.begin(), UD.end(),comp);
+    int res = 0;
+    for(pair<int,bool> x : UP) {
+        if(P - x.F >= 0)
+            P -= x.F;
+        else if(x.S)
+            res++;
+        else
+            res += 2;
+    }
+    for(pair<int,bool> x : UD) {
+        if(D - x.F >= 0) 
+            D -= x.F;
+        else if(x.S)
+            res++;
+        else
+            res += 2;
+    }
+    cout << res+Div;
 }
