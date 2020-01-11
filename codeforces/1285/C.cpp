63d7a066ca52982b5d7a83794ac2e02da4a5a903
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-01-11 18:12:00 +0100

    Add solution for problem `C - Fadi and LCM` Link: https://codeforces.com/contest/1285/problem/C Tags: brute force, math, number theory, *1400 Ref: https://codeforces.com/contest/1285/submission/68615115

--- codeforces/1285/C.cpp
+++ codeforces/1285/C.cpp
@@ -3,13 +3,21 @@ using namespace std;
 
 typedef long long ll;
 
+bool areCoprime(ll i, ll j) {
+    if(i > j) swap(i,j);
+    for(ll x=2; x<=i; x++)
+        if(i % x == 0 && j % x == 0)
+            return false;
+    return true;
+}
+
 int main() {
     ll N;
     cin >> N;
     for(ll i=sqrt(N); i>=1; i--) {
         if(N % i == 0) {
             ll j = N / i;
-            if(__gcd(i,j) == 1) {
+            if(areCoprime(i,j)) {
                 cout << i << " " << j << "\n";
                 return 0;
             } 
