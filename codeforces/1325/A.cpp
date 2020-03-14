6da1ffcea3ecda039ce44798fe3faacef9ee0747
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-03-14 15:40:00 +0100

    Add solution for problem `A - EhAb AnD gCd` Link: https://codeforces.com/contest/1325/problem/A Tags: constructive algorithms, greedy, number theory, *800 Ref: https://codeforces.com/contest/1325/submission/73227379

---
+++ codeforces/1325/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+#define F first
+#define S second
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        int x;
+        cin >> x;
+        cout << x-1 << " " << 1 << "\n";
+    }
+}
