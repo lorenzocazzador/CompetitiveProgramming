f15c9dfb8861ddc3feb872b9ff2d47238d2d92a9
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-03-20 14:53:00 +0100

    Add solution for problem `D1 - Prefix-Suffix Palindrome (Easy version)` Link: https://codeforces.com/contest/1326/problem/D1 Tags: hashing, string suffix structures, strings, *1500 Ref: https://codeforces.com/contest/1326/submission/73798795

--- codeforces/1326/D1.cpp
+++ codeforces/1326/D1.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize("O3")
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,6 +18,9 @@ bool isPal(string & s) {
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
     int T;
     cin >> T;
     while(T--) {
