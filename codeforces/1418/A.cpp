399f2bbc79ec99c51ffb965e2f16a86257314f48
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-09-14 16:51:00 +0200

    Add solution for problem `A - Buying Torches` Link: https://codeforces.com/contest/1418/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1418/submission/92802385

---
+++ codeforces/1418/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define db(x) cout << #x << ": " << x << "\n";
+
+void solve() {
+	ll X, Y, K;
+	cin >> X >> Y >> K;
+	cout << (Y * K + K - 1) / (X - 1) + ((Y * K + K - 1) % (X - 1) != 0) + K << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while(T--) solve();
+}
