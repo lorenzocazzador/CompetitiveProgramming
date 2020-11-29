e0fe68c599148d1e3abab0978abeda8c2f0b4e05
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-11-29 08:15:00 +0100

    Add solution for problem `A - Prison Break` Link: https://codeforces.com/contest/1457/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1457/submission/99845600

---
+++ codeforces/1457/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define db(x) cout << #x << ": " << x << "\n";
+
+void solve() {
+    int n, m, r, c;
+    cin >> n >> m >> r >> c;
+    cout << max({r-1+c-1, n-r+m-c, n-r+c-1, r-1+m-c}) << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while(T--) solve();
+}
