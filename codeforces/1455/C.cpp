4a07fd2316861251308afa9c119e751eb75ba230
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-11-30 16:06:00 +0100

    Add solution for problem `C - Ping-pong` Link: https://codeforces.com/contest/1455/problem/C Tags: constructive algorithms, games, math, *1100 Ref: https://codeforces.com/contest/1455/submission/100013238

---
+++ codeforces/1455/C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define db(x) cout << #x << ": " << x << "\n";
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    x--;
+    cout << x << ' ' << y << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while(T--) solve();
+}
