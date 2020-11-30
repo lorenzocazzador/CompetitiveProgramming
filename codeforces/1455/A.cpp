d23ba1dcaacab5856af54ed7ad271d962b7e4388
Lorenzo Cazzador <cazzadorlorenzo.1@gmail.com>
2020-11-30 15:39:00 +0100

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/99997124

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define db(x) cout << #x << ": " << x << "\n";
+
+void solve() {
+    string s;
+    cin >> s;
+    cout << sz(s) << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while(T--) solve();
+}
